f3d05bb9cbef0f43b808c0b74851b9a6411ec1a9
Yanming Zhou <zhouyanming@gmail.com>
2016-09-20 11:43:18 +0800

    Upgrade

--- src/com/demo/client/action/ProductAction.java
+++ src/com/demo/client/action/ProductAction.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.ironrhino.core.metadata.AutoConfig;
 import org.ironrhino.core.struts.BaseAction;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.demo.domain.Product;
@@ -13,6 +14,9 @@ import com.demo.service.ProductService;
 public class ProductAction extends BaseAction {
 
 	private static final long serialVersionUID = 7363154295606991023L;
+
+	@Autowired
+	private Logger logger;
 
 	@Autowired
 	private ProductService productService;
@@ -32,6 +36,7 @@ public class ProductAction extends BaseAction {
 	public String execute() {
 		String id = getUid();
 		if (id != null) {
+			logger.info("try find product {}", id);
 			product = productService.getById(id);
 			return VIEW;
 		} else {
